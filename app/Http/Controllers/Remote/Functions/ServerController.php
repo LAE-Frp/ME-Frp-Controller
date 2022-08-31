dddca7aa8b1d7d87a1f367a02320eef20e6440e3
iVampireSP.com <im@ivampiresp.com>
2022-08-31 17:30:26 +0800

    改进 安全问题

--- app/Http/Controllers/Remote/Functions/ServerController.php
+++ app/Http/Controllers/Remote/Functions/ServerController.php
@@ -10,7 +10,27 @@ class ServerController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $servers = Server::simplePaginate(10);
+        // 注意安全，一些字段可能对于用户来说是不可见的。
+        $servers = Server::select([
+            'id',
+            'name',
+            'allow_http',
+            'allow_https',
+            'allow_tcp',
+            'allow_udp',
+            'allow_stcp',
+
+            'min_port',
+            'max_port',
+
+            'tunnels',
+            'max_tunnels',
+
+            'status',
+
+            'price_per_gb',
+            'is_china_mainland'
+        ])->get();
 
         return $this->success($servers);
     }
