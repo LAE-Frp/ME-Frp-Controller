2516608834280bb53a1975fec9f9938c94db6528
iVampireSP.com <im@ivampiresp.com>
2022-09-06 21:51:49 +0800

    修复 中间件不生效的问题

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,6 +46,7 @@ class Kernel extends HttpKernel
 
         'remote' => [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Remote::class
         ]
     ];
 
@@ -66,7 +67,6 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'remote' => \App\Http\Middleware\Remote::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
     ];
 }
