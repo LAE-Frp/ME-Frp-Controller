a7cf448be01d98aa4b09c83b149beb7309f94ed8
iVampireSP.com <im@ivampiresp.com>
2022-09-06 23:35:54 +0800

    移除 多余的变量

--- resources/views/servers/index.blade.php
+++ resources/views/servers/index.blade.php
@@ -2,7 +2,6 @@
 
     <a href="{{ route('servers.create') }}">添加 Frps 服务器</a>
     <div class="list-group mt-3">
-        @php($user = auth()->user())
         @foreach ($servers as $server)
             <x-Server-View :server="$server" :url="route('servers.edit', $server->id)" />
         @endforeach
