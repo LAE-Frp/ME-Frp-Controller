ec2c70024c7829d3f3fe0c8e69512c9f6e88c4fa
iVampireSP.com <im@ivampiresp.com>
2022-09-13 21:26:40 +0800

    移除多余

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Host;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
-
 class HostController extends Controller
 {
     /**
